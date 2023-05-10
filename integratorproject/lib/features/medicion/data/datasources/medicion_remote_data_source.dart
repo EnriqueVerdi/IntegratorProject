60872c021119d400ff91c8e55bf932c61ab823cf
Marisol Solis Lopez <203411@ids.upchiapas.edu.mx>
2023-05-09 21:30:28 -0600

    Some changes

--- integratorproject/lib/features/medicion/data/datasources/medicion_remote_data_source.dart
+++ integratorproject/lib/features/medicion/data/datasources/medicion_remote_data_source.dart
@@ -5,20 +5,21 @@ import 'package:integratorproject/features/medicion/data/models/medicion_model.d
 
 abstract class MedicionRemoteDataSource {
   // https://jsonplaceholder.typicode.com/posts
-  Future<List<UserModel>> getMediciones();
+  Future<List<MedicionModel>> getMediciones();
 }
 
 class MedicionRemoteDataSourceImp implements MedicionRemoteDataSource {
   @override
-  Future<List<UserModel>> getMediciones() async {
+  Future<List<MedicionModel>> getMediciones() async {
     //print('DataSource');
-    var url = Uri.https('jsonplaceholder.typicode.com', '/posts');
+    var urlGet = '127.0.0.1/api/mediciones';
+    var url = Uri.https(urlGet, '/get');
     var response = await http.get(url);
 
     if (response.statusCode == 200) {
       return convert
           .jsonDecode(response.body)
-          .map<UserModel>((data) => UserModel.fromJson(data))
+          .map<MedicionModel>((data) => MedicionModel.fromJson(data))
           .toList();
     } else {
       throw Exception();
