f4be62bc6ea7d1c7051f245fdef125b759de66c0
Gabriel <gabrielramondev23@gmail.com>
2023-05-09 20:41:21 -0600

    user features/user/domain/entities

--- integratorproject/lib/features/user/domain/entities/user.dart
+++ integratorproject/lib/features/user/domain/entities/user.dart
@@ -1,9 +1,37 @@
 class User{
   final int id;
   final String name;
-
+  final String lastname;
+  final String email;
+  final String dayBirth;
+  final String nss;
+  final String sex;
+  final int age;
+  final double height;
+  final double weight;
+  final String phone;
+  final String ePhone;
+  final String adress;
+  final String nameMedic;
+  final String hospital;
+  final String password;
+  
   User({
     required this.id,
-    required this.name
+    required this.name,
+    required this.lastname,
+    required this.email,
+    required this.dayBirth,
+    required this.nss,
+    required this.sex,
+    required this.age,
+    required this.height,
+    required this.weight,
+    required this.phone,
+    required this.ePhone,
+    required this.adress,
+    required this.nameMedic,
+    required this.hospital,
+    required this.password
   });
 }
