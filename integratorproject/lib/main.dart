31acfff55a88dd9ca34cb08222fb71411c00511e
Marisol Solis Lopez <203411@ids.upchiapas.edu.mx>
2023-05-09 23:08:55 -0600

    main modified

--- integratorproject/lib/main.dart
+++ integratorproject/lib/main.dart
@@ -1,11 +1,92 @@
+// import 'package:flutter/material.dart';
+
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+// import 'usecase_config.dart';
+
+// UsecaseConfig usecaseConfig = UsecaseConfig();
+
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         // Here we take the value from the MyHomePage object that was created by
+//         // the App.build method, and use it to set our appbar title.
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         // Center is a layout widget. It takes a single child and positions it
+//         // in the middle of the parent.
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:integratorproject/usecase_config.dart';
 
-import 'usecase_config.dart';
+import 'features/medicion/presentation/blocs/mediciones_bloc.dart';
+import 'features/medicion/presentation/pages/mediciones_page.dart';
 
 UsecaseConfig usecaseConfig = UsecaseConfig();
-
 
 void main() {
   runApp(const MyApp());
@@ -16,63 +97,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<MedicionesBloc>(
+          create: (BuildContext context) => MedicionesBloc(getMedicionesUsecase: usecaseConfig.getMedicionesUsecase!)
         ),
+      ],
+
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MedicionesPage(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- integratorproject/lib/usecase_config.dart
+++ integratorproject/lib/usecase_config.dart
@@ -1,16 +1,29 @@
+import 'package:integratorproject/features/medicion/domain/repositoriess/medicion_repository.dart';
 
+import 'features/medicion/data/datasources/medicion_remote_data_source.dart';
+import 'features/medicion/data/repositories/medicion_repository_impl.dart';
+import 'features/medicion/domain/usecases/get_mediciones_usecase.dart';
 import 'features/user/data/datasources/user_remote_data_source.dart';
 import 'features/user/data/repositories/user_repository_impl.dart';
 import 'features/user/domain/usecases/get_users_usecase.dart';
 
 class UsecaseConfig {
-  GetUsersUsecase? getUsersUsecase;
-  UserRepositoryImpl? userRepositoryImpl;
-  UserRemoteDataSourceImp? userRemoteDataSourceImp;
+  // GetUsersUsecase? getUsersUsecase;
+  // UserRepositoryImpl? userRepositoryImpl;
+  // UserRemoteDataSourceImp? userRemoteDataSourceImp;
+
+  // UsecaseConfig() {
+  //   userRemoteDataSourceImp = UserRemoteDataSourceImp();
+  //   userRepositoryImpl = UserRepositoryImpl(userRemoteDataSource: userRemoteDataSourceImp!);
+  //   getUsersUsecase = GetUsersUsecase(userRepositoryImpl!);
+  GetMedicionesUsecase? getMedicionesUsecase;
+  MedicionRepositoryImpl? medicionRepositoryImpl;
+  MedicionRemoteDataSourceImp? medicionRemoteDataSourceImp;
 
   UsecaseConfig() {
-    userRemoteDataSourceImp = UserRemoteDataSourceImp();
-    userRepositoryImpl = UserRepositoryImpl(userRemoteDataSource: userRemoteDataSourceImp!);
-    getUsersUsecase = GetUsersUsecase(userRepositoryImpl!);
+    medicionRemoteDataSourceImp = MedicionRemoteDataSourceImp();
+    medicionRepositoryImpl = MedicionRepositoryImpl(
+        medicionRemoteDataSource: medicionRemoteDataSourceImp!);
+    getMedicionesUsecase = GetMedicionesUsecase(medicionRepositoryImpl!);
   }
 }
