fd21134a90c960e639c397948a427a958424da54
Marisol Solis Lopez <203411@ids.upchiapas.edu.mx>
2023-05-17 09:18:45 -0600

    Added estado

--- integratorproject/lib/features/tareas/data/models/tarea_model.dart
+++ integratorproject/lib/features/tareas/data/models/tarea_model.dart
@@ -1,15 +1,15 @@
 import 'package:integratorproject/features/tareas/domain/entities/tarea.dart';
 
 class TareaModel extends Tarea {
-  TareaModel({required int id, required String title, required String body})
-      : super(id: id, titulo: title, descripcion: body);
+  TareaModel({required int id, required String titulo, required String descripcion, required estado})
+      : super(id: id, titulo: titulo, descripcion: descripcion, estado: estado);
 
   factory TareaModel.fromJson(Map<String, dynamic> json) {
-    return TareaModel(id: json['id'], title: json['title'], body: json['body']);
+    return TareaModel(id: json['id'], titulo: json['titulo'], descripcion: json['descripcion'], estado: json['estado']);
   }
 
   factory TareaModel.fromEntity(Tarea tarea) {
     return TareaModel(
-        id: tarea.id, title: tarea.titulo, body: tarea.descripcion);
+        id: tarea.id, titulo: tarea.titulo, descripcion: tarea.descripcion, estado: tarea.estado);
   }
 }
